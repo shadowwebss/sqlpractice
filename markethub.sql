583d8a8b2cab2f7557dbbd876305c197531a9683
Vineet <vineetthakur2003aj@gmail.com>
2024-10-19 13:44:09 +0530

    create table queried for user tables

--- markethub.sql
+++ markethub.sql
@@ -1,1 +1,6 @@
 create database markethub;
+create table user(user_id int NOT NULL, user_name varchar(15) NOT NULL, user_phno int NOT NULL);
+create table address(address_id int NOT NULL, address_name varchar(15) NOT NULL, address_pincode int NOT NULL);
+alter table user add primary key(user_id);
+desc user;
+create table email(email_id int NOT NULL, firstname_name varchar(15) NOT NULL, last_name varchar(20) NOT NULL);

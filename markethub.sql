656f004b5a2135f8231fc39da02c92c3d21929ae
Shadow Web <rc2292062@gmail.com>
2024-10-19 13:41:17 +0530

    order quarry

--- markethub.sql
+++ markethub.sql
@@ -1,1 +1,5 @@
 create database markethub;
+
+create table order_id(user_id int NOT NULL,product_id int NOT NULL, name varchar(30), primary key (user_id)  );
+create table payment(user_id int NOT NULL, Payment_mode varchar(30),Payment_status varchar(30), primary key (user_id));
+create table shipping(user_id int NOT NULL, product_id int NOT NULL, Village varchar(30),City varchar(30),Pin_code int, primary key (user_id)  );

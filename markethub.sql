ee90778d4bfc3e4bb44044faeea127f59006f0d6
tejinder <tejinderchauhan9@gmail.com>
2024-10-19 13:52:43 +0530

    Create table for product

--- markethub.sql
+++ markethub.sql
@@ -1,1 +1,7 @@
 create database markethub;
+
+
+create table productid (productnumber int(10),producttype varchar(20));
+create table productdetails (userid int(10), productinformation varchar(20),producttype varchar(20));
+create table productdescription ( productcolour varchar(20), productsize int(10), roductweight varchar(20));
+create table productprice (currency varchar(10));

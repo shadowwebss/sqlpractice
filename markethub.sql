e7b79ef6855e0c65c25a9a764ae9a41e8697b00b
tejinder <tejinderchauhan9@gmail.com>
2024-10-21 13:42:52 +0530

    product

--- markethub.sql
+++ markethub.sql
@@ -15,3 +15,20 @@ create table email(email_id int NOT NULL, firstname_name varchar(15) NOT NULL, l
 create table order_id(user_id int NOT NULL,product_id int NOT NULL, name varchar(30), primary key (user_id)  );
 create table payment(user_id int NOT NULL, Payment_mode varchar(30),Payment_status varchar(30), primary key (user_id));
 create table shipping(user_id int NOT NULL, product_id int NOT NULL, Village varchar(30),City varchar(30),Pin_code int, primary key (user_id)  );
+
+
+insert into productid (productnumber, producttype) values (1, "eatables");
+insert into productid values(55,"fssfsf");
+insert into productid values(95,"fsssfsf");
+
+insert into productprice (currency) values ("10$");
+insert into productprice values("15$");
+insert into productprice values("20$");
+
+insert into productdetails (userid, productinformation, producttype) values (5, "veg", "solid");
+insert into productdetails values (7, "veg", "fragile");
+insert into productdetails values (9, "non-veg", "fatty");
+
+insert into productdescription values ("green", 12, "20kg");
+insert into productdescription values ("red", 10, "30kg");
+insert into productdescription values ("black", 40, "50kg");
